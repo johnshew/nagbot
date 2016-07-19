75ad34d0a10e626450cb010c6d6153b876179df7
billba <billba@microsoft.com>
2016-07-19 15:30:14 -0700

    experimental nag support

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 "use strict";
 const restify = require('restify');
-const builder = require('botbuilder');
+const botbuilder_1 = require('botbuilder');
 //=========================================================
 // Bot Setup
 //=========================================================
@@ -10,15 +10,24 @@ server.listen(process.env.port || process.env.PORT || 3978, () => {
     console.log('%s listening to %s', server.name, server.url);
 });
 // Create chat bot
-var connector = new builder.ChatConnector({
+var connector = new botbuilder_1.ChatConnector({
     appId: "0abb19bd-c099-4f45-a96d-ab8f9dde69c4" || process.env.MICROSOFT_APP_ID,
     appPassword: "GrFNjgGr6pUapgC67k8c5VX" || process.env.MICROSOFT_APP_PASSWORD
 });
-var bot = new builder.UniversalBot(connector);
+var bot = new botbuilder_1.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 //=========================================================
 // Bots Dialogs
 //=========================================================
+let address;
 bot.dialog('/', (session) => {
+    if (!address)
+        address = session.message.address;
     session.send("Hello World");
 });
+setInterval(() => {
+    var msg = new botbuilder_1.Message()
+        .address(address)
+        .text("Yo Yo");
+    bot.send(msg);
+}, 5000);
--- server.ts
+++ server.ts
@@ -1,7 +1,5 @@
-"use strict"
-
 import restify = require('restify');
-import builder = require('botbuilder');
+import { ChatConnector, UniversalBot, IAddress, Message } from 'botbuilder';
 
 //=========================================================
 // Bot Setup
@@ -15,18 +13,29 @@ server.listen(process.env.port || process.env.PORT || 3978, () => {
 });
 
 // Create chat bot
-var connector = new builder.ChatConnector({
+var connector = new ChatConnector({
     appId: "0abb19bd-c099-4f45-a96d-ab8f9dde69c4" || process.env.MICROSOFT_APP_ID,
     appPassword: "GrFNjgGr6pUapgC67k8c5VX" || process.env.MICROSOFT_APP_PASSWORD
 });
 
-var bot = new builder.UniversalBot(connector);
+var bot = new UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
+let address:IAddress;
+
 bot.dialog('/', (session) => {
+    if (!address)
+        address = session.message.address;
     session.send("Hello World");
 });
+
+setInterval(() => {
+    var msg = new Message()
+        .address(address)
+        .text("Yo Yo");
+    bot.send(msg);
+}, 5000);
