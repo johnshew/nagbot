6c4f9141031986013110cd9baf2393e737429a61
John Shew <johnshew@users.noreply.github.com>
2016-07-19 13:06:22 -0700

    Bot working

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
-'use strict'
+"use strict"
 
-// Load the http module to create an http server.
-var http = require('http');
+var restify = require('restify');
+var builder = require('botbuilder');
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer( (request, response) => {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+
+server.listen(process.env.port || process.env.PORT || 3798, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: "0abb19bd-c099-4f45-a96d-ab8f9dde69c4" || process.env.MICROSOFT_APP_ID,
+    appPassword: "GrFNjgGr6pUapgC67k8c5VX" || process.env.MICROSOFT_APP_PASSWORD
 });
 
-var port = process.env.port || 80;
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
 
-server.listen(port);
+//=========================================================
+// Bots Dialogs
+//=========================================================
 
-console.log(`Server running on ${ port }`);
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
