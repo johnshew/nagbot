d5b8bccefc4370827cb9af972a0a4b81b3deee17
John <johnshew@twitter.com>
2016-07-22 14:06:05 -0700

    Mixed prompts and LUIS

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-"use strict";
-const restify = require('restify');
-const botbuilder_1 = require('botbuilder');
+var restify = require('restify');
+var botbuilder_1 = require('botbuilder');
+var reminders = {};
 //=========================================================
 // Bot Setup
 //=========================================================
@@ -10,36 +10,82 @@ server.listen(process.env.port || process.env.PORT || 3978, () => {
     console.log('%s listening to %s', server.name, server.url);
 });
 // Create chat bot
-var connector = new botbuilder_1.ChatConnector({
+var cloudConnector = new botbuilder_1.ChatConnector({
     appId: process.env.BOT_APP_ID,
     appPassword: process.env.BOT_APP_PASSWORD
 });
-server.post('/api/messages', connector.listen());
+server.get(/\/public\/?.*/, restify.serveStatic({
+    directory: __dirname
+}));
+server.post('/api/messages', cloudConnector.listen());
 //=========================================================
 // Bots Dialogs
 //=========================================================
-var bot = new botbuilder_1.UniversalBot(connector);
+var bot = new botbuilder_1.UniversalBot(cloudConnector);
+// Bot global actions
+bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
+bot.beginDialogAction('help', '/help', { matches: /^help/i });
 bot.dialog('/', [
-        (session, args, next) => {
-        session.send("Hi There! What can I do for you?");
-        if (!session.userData.notificationAddresses) {
-            session.userData.notificationAddresses = [session.message.address];
-        }
-        next();
+    function (session) {
+        // Send a greeting and show help.
+        session.send("Hi... I will nag you until you do something.");
+        session.beginDialog('/help');
     },
-        (session, args, next) => {
-        session.beginDialog('/intent');
+    function (session, results) {
+        // Display menu
+        session.beginDialog('/menu');
+    },
+    function (session, results) {
+        // Always say goodbye
+        session.send("Ok... See you later!");
     }
 ]);
-bot.dialog('/foo', [
-        (session, args, next) => {
-        session.send("Hi");
-        session.endDialog();
-    }]);
+bot.dialog('/menu', [
+    function (session) {
+        botbuilder_1.Prompts.choice(session, "What would like to do?", "entry|list|(quit)");
+    },
+    function (session, results) {
+        if (results.response && results.response.entity != '(quit)') {
+            // Launch a dialog
+            session.beginDialog('/' + results.response.entity);
+        }
+        else {
+            // Exit the menu
+            session.endDialog();
+        }
+    },
+    function (session, results) {
+        // The menu runs a loop until the user chooses to (quit).
+        session.replaceDialog('/menu');
+    }
+]).reloadAction('reloadMenu', null, { matches: /^menu|show menu/i });
+bot.dialog('/help', [
+    function (session) {
+        session.endDialog("You can give me the following commands:\n\n* menu - returns to the menu.\n* goodbye - Eed this conversation.\n* help - Displays these commands.");
+    }
+]);
+bot.dialog('/list', [
+        (session) => {
+        session.send("Here are your current reminders.");
+        Object.keys(reminders).forEach((key) => {
+            session.send(`${key} at ${reminders[key].timestamp} `);
+        });
+        session.endDialog("Done listing.");
+    }
+]);
+bot.dialog('/entry', [
+        (session) => {
+        session.send("Please enter a command using natural language.");
+        session.beginDialog('/freeform');
+    },
+        (session) => {
+        session.endDialog("Done with freeform entry.");
+    }
+]);
 var model = process.env.BOT_APP_LUIS_CORTANA_RECOGNIZER;
 var recognizer = new botbuilder_1.LuisRecognizer(model);
 var intentDialog = new botbuilder_1.IntentDialog({ recognizers: [recognizer] });
-bot.dialog('/intent', intentDialog);
+bot.dialog('/freeform', intentDialog);
 intentDialog.matches('builtin.intent.reminder.create_single_reminder', [
         (session, args, next) => {
         var title = botbuilder_1.EntityRecognizer.findEntity(args.entities, 'builtin.reminder.title');
@@ -50,20 +96,51 @@ intentDialog.matches('builtin.intent.reminder.create_single_reminder', [
         };
         // Prompt for title
         if (!reminder.title) {
-            botbuilder_1.Prompts.text(session, 'What would you like to call your alarm?');
+            botbuilder_1.Prompts.text(session, 'What would you like to call your reminder?');
         }
         else {
             next();
         }
-        session.send("Got alarm");
+    },
+        (session, results, next) => {
+        var reminder = session.dialogData.reminder;
+        if (results.response) {
+            reminder.title = results.response;
+        }
+        // Prompt for time (title will be blank if the user said cancel)
+        if (reminder.title && !reminder.timestamp) {
+            botbuilder_1.Prompts.time(session, 'What time would you like to set the reminder for?');
+        }
+        else {
+            next();
+        }
+    },
+        (session, results) => {
+        var reminder = session.dialogData.reminder;
+        if (results.response) {
+            var time = botbuilder_1.EntityRecognizer.resolveTime([results.response]);
+            reminder.timestamp = time ? time.getTime() : null;
+        }
+        // Set the reminder (if title or timestamp is blank the user said cancel)
+        if (reminder.title && reminder.timestamp) {
+            // Save address of who to notify and write to scheduler.
+            reminder.address = session.message.address;
+            reminders[reminder.title] = reminder;
+            // Send confirmation to user
+            var date = new Date(reminder.timestamp);
+            var isAM = date.getHours() < 12;
+            session.send('Creating reminder named "%s" for %d/%d/%d %d:%02d%s', reminder.title, date.getMonth() + 1, date.getDate(), date.getFullYear(), isAM ? date.getHours() : date.getHours() - 12, date.getMinutes(), isAM ? 'am' : 'pm');
+        }
+        else {
+            session.send('Ok... no problem.');
+        }
     }
 ]);
-intentDialog.onDefault(botbuilder_1.DialogAction.send("Sorry I didn't understand you. I can only create & delete alarms."));
-setInterval(() => {
+intentDialog.onDefault(botbuilder_1.DialogAction.send("I can only create reminders.   For example: create a reminder for tomorrow at noon named test"), setInterval(() => {
     /*
     var msg = new Message()
         .address(session.userData.notificationAddresses[0])
         .text("tick-tock");
     bot.send(msg);
     */
-}, 5000);
+}, 5000));
--- server.ts
+++ server.ts
@@ -1,5 +1,9 @@
+
 import restify = require('restify');
-import { ChatConnector, UniversalBot, IAddress, Message, LuisRecognizer, IntentDialog, DialogAction, EntityRecognizer, Prompts } from 'botbuilder';
+import { ConsoleConnector, ChatConnector, UniversalBot, IAddress, Message, LuisRecognizer, IntentDialog, DialogAction, EntityRecognizer, Prompts } from 'botbuilder';
+
+var reminders = {};
+
 
 //=========================================================
 // Bot Setup
@@ -13,68 +17,154 @@ server.listen(process.env.port || process.env.PORT || 3978, () => {
 });
 
 // Create chat bot
-var connector = new ChatConnector({
+var cloudConnector = new ChatConnector({
     appId: process.env.BOT_APP_ID,
     appPassword: process.env.BOT_APP_PASSWORD
 
 });
 
 
-server.post('/api/messages', connector.listen());
+server.get(/\/public\/?.*/, restify.serveStatic({
+    directory: __dirname
+}));
+
+server.post('/api/messages', cloudConnector.listen());
+
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-var bot = new UniversalBot(connector);
+var bot = new UniversalBot(cloudConnector);
+
+// Bot global actions
+bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
+bot.beginDialogAction('help', '/help', { matches: /^help/i });
 
 bot.dialog('/', [
-    (session, args, next) => {
-        session.send("Hi There! What can I do for you?");
-        if (!session.userData.notificationAddresses) {
-            session.userData.notificationAddresses = [session.message.address];
-        }
-        next();
+    function (session) {
+        // Send a greeting and show help.
+        session.send("Hi... I will nag you until you do something.");
+        session.beginDialog('/help');
     },
-    (session, args, next) => {
-        session.beginDialog('/intent');
+    function (session, results) {
+        // Display menu
+        session.beginDialog('/menu');
+    },
+    function (session, results) {
+        // Always say goodbye
+        session.send("Ok... See you later!");
     }
 ]);
 
+bot.dialog('/menu', [
+    function (session) {
+        Prompts.choice(session, "What would like to do?", "entry|list|(quit)");
+    },
+    function (session, results) {
+        if (results.response && results.response.entity != '(quit)') {
+            // Launch a dialog
+            session.beginDialog('/' + results.response.entity);
+        } else {
+            // Exit the menu
+            session.endDialog();
+        }
+    },
+    function (session, results) {
+        // The menu runs a loop until the user chooses to (quit).
+        session.replaceDialog('/menu');
+    }
+]).reloadAction('reloadMenu', null, { matches: /^menu|show menu/i });
 
-bot.dialog('/foo', [
-    (session, args, next) => {
-        session.send("Hi");
-        session.endDialog();
-    }])
+bot.dialog('/help', [
+    function (session) {
+        session.endDialog("You can give me the following commands:\n\n* menu - returns to the menu.\n* goodbye - Eed this conversation.\n* help - Displays these commands.");
+    }
+]);
+
+bot.dialog('/list', [
+     (session) => {
+        session.send("Here are your current reminders.");
+        Object.keys(reminders).forEach((key)=>{ 
+            session.send(`${ key } at ${ reminders[key].timestamp} `); 
+        });
+        session.endDialog("Done listing.");
+    }
+]);
+
+bot.dialog('/entry', [
+    (session) => {
+        session.send("Please enter a command using natural language.");
+        session.beginDialog('/freeform');
+    },
+    (session) => {
+        session.endDialog("Done with freeform entry.")
+    }
+]);
 
 
 var model = process.env.BOT_APP_LUIS_CORTANA_RECOGNIZER;
 var recognizer = new LuisRecognizer(model);
 var intentDialog = new IntentDialog({ recognizers: [recognizer] });
 
-bot.dialog('/intent', intentDialog);
+bot.dialog('/freeform', intentDialog);
 
 intentDialog.matches('builtin.intent.reminder.create_single_reminder', [
     (session, args, next) => {
         var title = EntityRecognizer.findEntity(args.entities, 'builtin.reminder.title');
         var time = EntityRecognizer.resolveTime(args.entities);
         var reminder = session.dialogData.reminder = {
-          title: title ? title.entity : null,
-          timestamp: time ? time.getTime() : null  
+            title: title ? title.entity : null,
+            timestamp: time ? time.getTime() : null
         };
-        
+
         // Prompt for title
         if (!reminder.title) {
-            Prompts.text(session, 'What would you like to call your alarm?');
+            Prompts.text(session, 'What would you like to call your reminder?');
         } else {
             next();
         }
-        session.send("Got alarm");
+    },
+    (session, results, next) => {
+        var reminder = session.dialogData.reminder;
+        if (results.response) {
+            reminder.title = results.response;
+        }
+
+        // Prompt for time (title will be blank if the user said cancel)
+        if (reminder.title && !reminder.timestamp) {
+            Prompts.time(session, 'What time would you like to set the reminder for?');
+        } else {
+            next();
+        }
+    },
+    (session, results) => {
+        var reminder = session.dialogData.reminder;
+        if (results.response) {
+            var time = EntityRecognizer.resolveTime([results.response]);
+            reminder.timestamp = time ? time.getTime() : null;
+        }
+
+        // Set the reminder (if title or timestamp is blank the user said cancel)
+        if (reminder.title && reminder.timestamp) {
+            // Save address of who to notify and write to scheduler.
+            reminder.address = session.message.address;
+            reminders[reminder.title] = reminder;
+
+            // Send confirmation to user
+            var date = new Date(reminder.timestamp);
+            var isAM = date.getHours() < 12;
+            session.send('Creating reminder named "%s" for %d/%d/%d %d:%02d%s',
+                reminder.title,
+                date.getMonth() + 1, date.getDate(), date.getFullYear(),
+                isAM ? date.getHours() : date.getHours() - 12, date.getMinutes(), isAM ? 'am' : 'pm');
+        } else {
+            session.send('Ok... no problem.');
+        }
     }
 ]);
 
-intentDialog.onDefault(DialogAction.send("Sorry I didn't understand you. I can only create & delete alarms."));
+intentDialog.onDefault(DialogAction.send("I can only create reminders.   For example: create a reminder for tomorrow at noon named test")
 
 
 setInterval(() => {
